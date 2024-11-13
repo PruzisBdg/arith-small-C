4ef18fff28be4f75021e40b6c8478afd0fee3eaa
pruzisp51 <spjh1@wi.rr.com>
2024-11-12 21:45:50 -0600

    Adds STM32CubeIDE managed library build 'arith_stm32_arm_gcc'

--- private/libs_support.h
+++ private/libs_support.h
@@ -3,10 +3,17 @@
 
 #include "GenericTypeDefs.h"
 
-#define TOOL_CC430 1
+#define TOOL_CC430   1
+#define TOOL_GCC_ARM 2
 
 #ifdef __TOOL_IS_CC430
    #define _TOOL_IS TOOL_CC430
+#else
+   #ifdef __TOOL_IS_GCC_ARM
+      #define _TOOL_IS TOOL_GCC_ARM
+   #else
+      #error "_TOOL_IS must be defined"
+   #endif
 #endif
 
 #endif // LIBS_SUPPORT_H
